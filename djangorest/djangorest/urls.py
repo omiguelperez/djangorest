700697b20e28a4ae8187069d8f35dbc1210751d0
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-05-16 07:24:15 -0500

    Organizar urls del proyecto

--- djangorest/djangorest/urls.py
+++ djangorest/djangorest/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from api.views import CreateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^bucketlist/$', CreateView.as_view(), name='create'),
+    url(r'^', include('api.urls')),
 ]
