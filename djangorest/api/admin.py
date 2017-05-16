7c24b14ec8f3476322b5ee83a20054ae063ae598
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-05-16 09:52:07 -0500

    Agregar `onwer` al bucketlist y registrar en el admin

--- djangorest/api/admin.py
+++ djangorest/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from .models import BucketList
+
+
+class BucketListAdmin(ModelAdmin):
+    list_display = ('name', 'owner', 'date_created', 'date_modified')
+    list_filter = ('owner',)
+    search_fields = ('name',)
+
+
+admin.site.register(BucketList, BucketListAdmin)
--- djangorest/api/models.py
+++ djangorest/api/models.py
@@ -1,7 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class BucketList(models.Model):
+    """This class represents the bucketlist model."""
     name = models.CharField(max_length=40, default='Unnamed')
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(
+        User,
+        related_name='bucketlists',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return '{}'.format(self.name)
